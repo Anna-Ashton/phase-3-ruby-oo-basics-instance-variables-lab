6a641d5af73490dee2ceba5a3726c3137fd2cae0
Anna-Ashton <ann.karanja@student.moringaschool.com>
2022-10-21 12:22:51 +0300

    Completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
+    #instance method 'name='
+    #takes in argument dog_name
+    def name=(dogs_name)
+        @this_dogs_name = dogs_name
+    end
+
+    #second instance method responsible for reporting the name
+    def name
+            @this_dogs_name
+    end
 
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+ puts lassie.name
